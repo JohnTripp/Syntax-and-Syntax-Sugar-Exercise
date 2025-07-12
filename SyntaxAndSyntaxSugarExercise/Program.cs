e12dd8b10082d56c8eabf980c9523bb89b518812
John Tripp <madfish13@hotmail.com>
2025-07-11 20:31:54 -0400

    done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 10;
+            var response = answer < 9 ? $"{answer} is less than 9" : $"{answer} is more than or equal to 9";
+            Console.WriteLine(response);
+
         }
     }
 }
